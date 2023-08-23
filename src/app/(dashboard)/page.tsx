00dc8450158a4065afc41eb6a86fe0bea38e11b6
Khanh <khanhtranngoccva@gmail.com>
2023-08-23 09:14:16 +0700

    feat: Add auth deps

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 
 export default async function Dashboard() {
     const data = await scanModules();
-    return <div className={`flex justify-center overflow-y-scroll overflow-x-hidden flex-1 my-[-8px] pt-[8px] pr-[12px]`}>
+    return <div className={`flex justify-center overflow-y-scroll overflow-x-hidden flex-1 my-[-8px] py-[16px] pr-[12px]`}>
         <div className={`max-w-[12  0rem] flex-col flex gap-12`}>
             {data.map(item => {
                 return <DashboardModule key={item.slug} moduleInfo={item}></DashboardModule>;
