5f60fcd5db753f1740757a2fed1b0f0351a86a08
Khanh Tran <khanhtranngoccva@gmail.com>
2023-08-26 14:58:57 +0700

    feat: Sentry token

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://b2eb7efb1225e1f620f496df014d6112@o4505770167697408.ingest.sentry.io/4505770167762944",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
